9bb0f48e4d85adc9c6ce52d485f70aeb355d44c2
dharunsrinivasan05-prog <dharunsrinivasan05@gmail.com>
2026-04-05 20:48:12 +0530

    Add files via upload

---
+++ HelloApp.java
@@ -0,0 +1,12 @@
+package hidharun;
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        // Assign name using ternary operator
+        String name = (args.length > 0) ? args[0] : "World";
+
+        // Display greeting
+        System.out.println("Hello, " + name + "!");
+    }
+}
